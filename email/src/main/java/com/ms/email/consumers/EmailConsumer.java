d8c1f34df14242cd1950e8fb190c7ca80b0010df
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-12-27 23:33:18 -0400

    Added email service send email event method

--- email/src/main/java/com/ms/email/consumers/EmailConsumer.java
+++ email/src/main/java/com/ms/email/consumers/EmailConsumer.java
@@ -1,9 +1,8 @@
 package com.ms.email.consumers;
 
-//import com.ms.email.dtos.EmailRecordDto;
-//import com.ms.email.models.EmailModel;
-//import com.ms.email.services.EmailService;
 import com.ms.email.dtos.EmailRecordDto;
+import com.ms.email.models.EmailModel;
+import com.ms.email.services.EmailService;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.beans.BeanUtils;
 import org.springframework.messaging.handler.annotation.Payload;
@@ -12,18 +11,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class EmailConsumer {
 
-//    final EmailService emailService;
-//
-//    public EmailConsumer(EmailService emailService) {
-//        this.emailService = emailService;
-//    }
+    final EmailService emailService;
 
-    @RabbitListener(queues = "${broker.queue.email.name}" )
+    public EmailConsumer(EmailService emailService) {
+        this.emailService = emailService;
+    }
+
+    @RabbitListener(queues = "${broker.queue.email.name}")
     public void listenEmailQueue(@Payload EmailRecordDto emailRecordDto) {
-//        var emailModel = new EmailModel();
-//        BeanUtils.copyProperties(emailRecordDto, emailModel);
-//        emailService.sendEmail(emailModel);
-        System.out.println(emailRecordDto.emailTo());
+        var emailModel = new EmailModel();
+        BeanUtils.copyProperties(emailRecordDto, emailModel);
+        emailService.sendEmail(emailModel);
+
+        System.out.println("An email was send to " + emailRecordDto.emailTo());
     }
 
 }
