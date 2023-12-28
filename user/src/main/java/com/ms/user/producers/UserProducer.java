081aabecd87cfa8c4be79b05d9a95dc01e2d75bd
Leandro Santiago <leandro.s4nt1@gmail.com>
2023-12-27 22:56:12 -0400

    Added user send email flow

---
+++ user/src/main/java/com/ms/user/producers/UserProducer.java
@@ -0,0 +1,32 @@
+package com.ms.user.producers;
+
+import com.ms.user.dtos.EmailDto;
+import com.ms.user.models.UserModel;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class UserProducer {
+
+    final RabbitTemplate rabbitTemplate;
+
+    public UserProducer(RabbitTemplate rabbitTemplate) {
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Value(value = "${broker.queue.email.name}")
+    private String routingKey;
+
+    public void publishMessageEmail(UserModel userModel) {
+        var emailDto = new EmailDto();
+        emailDto.setUserId(userModel.getUserId());
+        emailDto.setEmailTo(userModel.getEmail());
+        emailDto.setSubject("Registered Successfully!");
+        emailDto.setText(userModel.getName() + ", Welcome! \nThanks for register in our platform" +
+                ", now you can use as you want!");
+
+        rabbitTemplate.convertAndSend("", routingKey, emailDto); // "" uses exchange default from rabbit
+    }
+
+}
